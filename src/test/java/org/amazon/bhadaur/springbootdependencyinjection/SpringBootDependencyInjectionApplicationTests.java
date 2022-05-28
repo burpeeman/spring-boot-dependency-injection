832d752fa450f3fa5dadcc38143129c463135b1a
burpeeman <55798817+burpeeman@users.noreply.github.com>
2022-05-28 21:02:30 +0530

    Initial commit

---
+++ src/test/java/org/amazon/bhadaur/springbootdependencyinjection/SpringBootDependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.amazon.bhadaur.springbootdependencyinjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDependencyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
