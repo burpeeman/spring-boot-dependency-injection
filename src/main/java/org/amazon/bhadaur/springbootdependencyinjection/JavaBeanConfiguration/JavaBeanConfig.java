54f267f12ac3d0ed2eccdb3f357b82b2cd95dd96
Rishabh Bhadauria <bhadaur@amazon.com>
2022-06-15 08:22:19 +0530

    Modified code to get message based on application profiles

--- src/main/java/org/amazon/bhadaur/springbootdependencyinjection/JavaBeanConfiguration/JavaBeanConfig.java
+++ src/main/java/org/amazon/bhadaur/springbootdependencyinjection/JavaBeanConfiguration/JavaBeanConfig.java
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
-@PropertySource("classpath:messages.properties")
 @Configuration
 public class JavaBeanConfig {
 
